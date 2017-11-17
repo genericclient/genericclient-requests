a8f0dbe8e9591aa4bfb4a58ad0c66feb86060c61
Flavio Curella <flavio.curella@gmail.com>
2017-11-17 15:55:06 -0600

    Bump version: 0.0.21 → 0.0.22

--- genericclient/__init__.py
+++ genericclient/__init__.py
@@ -3,7 +3,7 @@ import requests
 from genericclient_base import BaseEndpoint, BaseGenericClient, BaseResource, exceptions
 
 
-_version = "0.0.21"
+_version = "0.0.22"
 __version__ = VERSION = tuple(map(int, _version.split('.')))
 
 
